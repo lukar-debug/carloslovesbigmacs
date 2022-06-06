6c67882a52e818b8879a4a36a28837281049b1a1
Luis Perez <84640224+lukar-debug@users.noreply.github.com>
2022-06-05 20:44:08 -0400

    carlos is fat
    carlos is fat again

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('test carlos is fat lol');
+alert("carlos sucks amrite");
+
+var  test 
+test = 'carlos'
+
+//document.write (test)--!!>
+
+function greet (firstname, lastname) {
+    console.log("hi "+ firstname +  " "+ lastname)
+
+}
+greet("Joe", "MAMA")
+
+
+
+function square(number){
+    return number * number
+
+}
+let number = square(9);
+console.log(number);
